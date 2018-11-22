a7843919ccc763d1569a029f573e0ac398a6229f
2ba2 <twobatwo@126.com>
2018-11-22 14:58:23 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'FE notes',
+    description: 'Just playing around',
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/guide/' },
+        { text: 'External', link: 'https://google.com' },
+      ]
+    }    
+  }
